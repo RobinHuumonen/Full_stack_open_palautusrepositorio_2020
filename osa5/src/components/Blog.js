6fa202aa8f656206ca6f02238ee16e56c2d4287f
RobinHuumonen <robin.huumonen@hotmail.com>
2020-06-24 17:30:29 +0300

    5.7* blogilistan frontend, step7

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const toggleVisibility = () => {
+    setShowAll(!showAll)
+  }
+
+  const hideWhenVisible = { display: showAll ? 'none' : ''}
+  const showWhenVisible = { display: showAll ? '' : 'none'}
+
+  return (
+    <div className="blog">
+      <div style={hideWhenVisible}>
+        {blog.title} 
+        {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title}
+        <button onClick={toggleVisibility}>hide</button>
+        <br/>
+        {blog.url}
+        <br/>
+        {blog.likes}
+        <button>likes</button>
+        <br/>
+        {blog.author}
+      </div>
+    </div>
+
+  )
+}
+
 
 export default Blog
