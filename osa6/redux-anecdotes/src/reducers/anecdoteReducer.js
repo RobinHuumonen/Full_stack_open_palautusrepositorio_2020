dbcdb0bf9e945cf03e518b468f6afbf6997bb077
RobinHuumonen <robin.huumonen@hotmail.com>
2020-07-11 12:58:03 +0300

    6.15 anekdootit ja backend, step3

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { act } from "react-dom/test-utils"
+import anecdoteService from '../services/anecdotes'
 
 const anecdotesAtStart = [
   'If it hurts, do it more often',
@@ -36,13 +37,15 @@ const reducer = (state = [], action) => {
   }
 }
 
-export const initAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
-
 export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
